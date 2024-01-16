3f04b05f40a8ec37abf8c095fa18b3cb3817bdfc
xiaohuohumax <xiaohuohumax@users.noreply.github.com>
2024-01-16 09:31:13 +0000

    deploy: xiaohuohumax/MiaoJi@bd3856dbcae8c0a2f9a1226d706288d012eaff3e

---
+++ assets/CSubTitle.vue_vue_type_script_setup_true_lang-014bf22b.js
@@ -0,0 +1,1 @@
+import{d as e,af as t,ai as o,ah as s,am as r,an as n,ao as c}from"./index-a5b419ed.js";import{N as i}from"./Space-4e5bd7da.js";const l={class:"font-bold text-lg"},m=e({__name:"CSubTitle",props:{context:{}},setup(p){return(a,_)=>(t(),o(c(i),{size:"small",bordered:!1},{default:s(()=>[r("div",l,n(a.context),1)]),_:1}))}});export{m as _};
